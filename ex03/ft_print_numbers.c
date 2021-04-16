d21050087f93bace7e99f736a8eba1628ceae28b
mcamarin <mcamarin@student.42sp.org.br>
2021-04-16 19:09:03 +0000

    Ficha C00 basecamp42

---
+++ ex03/ft_print_numbers.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcamarin <mcamarin@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/04 04:37:38 by mcamarin          #+#    #+#             */
+/*   Updated: 2021/04/06 13:18:39 by mcamarin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char n)
+{
+	write(1, &n, 1);
+}
+
+void	ft_print_numbers(void)
+{
+	char numeros;
+
+	numeros = '0';
+	while (numeros <= '9')
+	{
+		ft_putchar(numeros);
+		numeros++;
+	}
+}
